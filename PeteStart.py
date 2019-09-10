157d1dcbf8d853122e8b82cf13bc6eae7a017764
Pete George <georgepj@vcu.edu>
2019-09-10 15:27:01 -0400

    Plotting on one graph, looks odd though

--- PeteStart.py
+++ PeteStart.py
@@ -1,19 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('groupA.txt', header=None, names = ['Height', 'Weight', 'Gender'])
-print(df)
+dfA = pd.read_csv('groupA.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfB = pd.read_csv('groupB.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfC = pd.read_csv('groupC.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+
+'''Appending the data groups from files'''
+dfA = dfA.append(dfB, ignore_index=True)
+dfA = dfA.append(dfC, ignore_index=True)
+
+print(dfA)
+
 
 '''Splits the original bulk data into two'''
 '''https://stackoverflow.com/questions/27900733/python-pandas-separate-a-dataframe-based-on-a-column-value'''
-males = df[df.Gender == 0]
-females = df[df.Gender == 1]
+males = dfA[dfA.Gender == 0]
+females = dfA[dfA.Gender == 1]
+print(females)
 
-X = males["Height"].tolist() '''maybe use'''
-
-'''currently as 2 separate plots'''
-males.plot(kind='scatter',x='Weight',y='Height',color='blue')
-females.plot(kind='scatter',x='Weight',y='Height',color='red')
 
 
 '''Need to NORMALIZE data!'''
+'''For normalizing maybe 
+    Data is already normalized in plot?'''
+    
+minH = dfA['Height'].min()
+minW = dfA['Weight'].min()
+maxH = dfA['Height'].max()
+maxW = dfA['Weight'].max()
+'''How to manipulate DF values now'''
+
+
+'''Plotting the Data'''
+plt.scatter(females['Weight'], females['Height'], color='r', marker='.')
+
+plt.scatter(males['Weight'], males['Height'], color='b', marker='.')
+
+plt.show()
+'''Need to add groupB/C.txt data'''
+
