7f93e7b6809283d463a9413391be910d6c1e64dd
Peter J George <42278219+pjgeorge09@users.noreply.github.com>
2019-10-01 18:27:41 -0400

    Add files via upload

--- part2.py
+++ part2.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import math
 
 '''A method to normalize sets of data to values [0-1]'''
 def normalizeData (aPanda):
@@ -39,121 +40,143 @@ def netCalc(array1, array2):
     sum += array1[2]                         #Adding in the bias
     # If this value is greater than zero , fire neuron otherwise don't
     
-    sum = round(sum,6)
     return sum
 
-def SAF(orig, train, p):
-    u=2000
-    cycles = 100
-    r,c = train.shape                               #r=rows c=columns
-    r = r/2                                         #half because we do 1M1W at a time
-    r = round(r * p)                                #Make sure it's an int
-    alpha = 0.1                                     #Static Alpha
-    TE = 0
-    
-    for i in range (0,cycles):                      #For number of cycles
-        print(i)
-        print(orig)
-        for k in range (0,r):                       #For each row we are doing
-            '''One Male'''
-            row = train.iloc[k]                     #Make it a row object
-            pArray=[]
-            for l in row:
-                pArray.append(l)                    #Make row object a simple array
-            net = 0                                 #Init net
-            for j in range (0,3):                   #For every value of the array
-                net = net + pArray[j]*orig[j]       #Net += (A * B) per notes slide 13 pp11/12
-            out = 1                                 #init out
-            if (net < 0):
-                out = -1                            #Change out if net < 0 (maybe out = 0 because unipolar)
-            error = pArray[2] - out                  #Error = desired output minus actual output (0 or 1)
-            learn = alpha*error                     #Learn = alpha * error per same slide
-            for j in range(0,3):                    
-                orig[j] = orig[j] + learn*pArray[j] #Update Weight by adding learn * pattern
+
+def SAF(originalArray, trainingSet, p, epsilon, alpha, gain):
+    ni = 5000
+    fire = 0 #initialize for reset/global use in function, "did neuron fire"
+    r,c = trainingSet.shape
+    #r = r/2 #Doing one male one female each time in for loop
+    #r = round(r * p) #Make sure it's an int
+    u = 2000 # To offset for female sampling
+    # It will always do this exactly one time for a male, and one time for a female
+    TE = 2000
+    while(TE > epsilon):
+        for bigNum in range (0,100):
+            TE = 0
+            print(bigNum)
+            #print(str(originalArray[0]) + "X * " + str(originalArray[1]) + "Y + " + str(originalArray[2]) + " > 0")  
+            for x in range(0,u-1): #statically trying different values for data update
+                fire = 0
+                '''For 1 male'''
+                #ex = random.randrange(0,1999)
+                pattern = trainingSet.iloc[x] #should be one row of the passed data set
+                #this block just makes the row into an array
+                pArray = []
+                for j in pattern:
+                    pArray.append(j)
+                # net is  the net from slides, where it's >= -1  (midslide pp07)
+                try:
+                    out = 1/(1+(math.exp(-1*gain*netCalc(originalArray,pArray))))
+                except OverflowError:
+                    out = pArray[2]
+                fire = out
+                delta = pArray[2] - fire 
+                for s in range (0,2):
+                    originalArray[s] += delta*alpha*pArray[s]
+                originalArray[2] += delta*alpha
+                TE += delta**2
             
-            '''One Female'''
-            row = train.iloc[k+u]                   #Make it a row object
-            pArray2=[]
-            for l in row:
-                pArray2.append(l)                    #Make row object a simple array
-            net = 0                                 #Init net
-            for j in range (0,3):                   #For every value of the array
-                net = net + pArray2[j]*orig[j]       #Net += (A * B) per notes slide 13 pp11/12
-            out = 1                                 #init out
-            if (net < 0):
-                out = -1                            #Change out if net < 0 (maybe out = 0 because unipolar)
-            error = pArray2[2] - out                  #Error = desired output minus actual output (0 or 1)
-            learn = alpha*error                     #Learn = alpha * error per same slide
-            for j in range(0,3):                    
-                orig[j] = orig[j] + learn*pArray2[j] #Update Weight by adding learn * pattern
-            
-            
-            
-    return
-    
+                '''For 1 female       NOTE BELOW THIS'''
+                fire = 0
+                pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
+                pArray2 = []
+                for j in pattern2:
+                    pArray2.append(j)
+                try:
+                    out = 1/(1+(math.exp(-gain*netCalc(originalArray,pArray2))))
+                except OverflowError:
+                    out = pArray2[2]
+                fire = out
+                delta = pArray2[2] - fire
+                for s in range (0,2):
+                    originalArray[s] += delta*alpha*pArray2[s]
+                originalArray[2] += delta*alpha            
+                TE += delta**2
+            print(TE)
+                
+            if(TE<epsilon or bigNum == 99):
+                break
+    return originalArray
+
 
 
 '''                 Hard Activation Function Method                         '''
 ''' @input originalArray = is the starting weight array, initialized elsewhere
     @input trainingSet = is the set of data, dfA,dfB,dfC for us in our work
     @input p = is the percent of the data you want to use as training data'''
-def HAF(originalArray, trainingSet, p):
+def HAF(originalArray, trainingSet, p, epsilon, alpha):
+    ni = 5000
     fire = 0 #initialize for reset/global use in function, "did neuron fire"
     r,c = trainingSet.shape
-    r = r/2 #Doing one male one female each time in for loop
-    r = round(r * p) #Make sure it's an int
+    #r = r/2 #Doing one male one female each time in for loop
+    #r = round(r * p) #Make sure it's an int
     u = 2000 # To offset for female sampling
     # It will always do this exactly one time for a male, and one time for a female
-    for bigNum in range (0,100):
-        #print(bigNum)
-        #print(str(originalArray[0]) + "X * " + str(originalArray[1]) + "Y + " + str(originalArray[2]) + " > 0")  
-        for x in range(0,r): #statically trying different values for data update
-            fire = 0
-            '''For 1 male'''
-            pattern = trainingSet.iloc[x] #should be one row of the passed data set
-            #this block just makes the row into an array
-            pArray = []
-            for j in pattern:
-                pArray.append(j)
-            # net is  the net from slides, where it's >= -1  (midslide pp07)
-            net = netCalc(originalArray, pArray) #USE PP4 SLIDE 3,
-            #Activation function
-            if (net > 0):
-                fire = 1
-            elif (net <= 0 ):
+    TE = 2000
+    while(TE > epsilon):
+        for bigNum in range (0,ni):
+            TE = 0
+            print(bigNum)
+            #print(str(originalArray[0]) + "X * " + str(originalArray[1]) + "Y + " + str(originalArray[2]) + " > 0")  
+            for x in range(0,1999): #statically trying different values for data update
                 fire = 0
-            delta = pArray[2] - fire 
-            for s in range (0,2):
-                originalArray[s] += delta*alpha*pArray[s]
-            originalArray[2] += delta*alpha
+                '''For 1 male'''
+                #ex = random.randrange(0,1999)
+                pattern = trainingSet.iloc[x] #should be one row of the passed data set
+                #this block just makes the row into an array
+                pArray = []
+                for j in pattern:
+                    pArray.append(j)
+                # net is  the net from slides, where it's >= -1  (midslide pp07)
+                net = netCalc(originalArray, pArray) #USE PP4 SLIDE 3,
+                #Activation function
+                if (net > 0):
+                    fire = 1
+                elif (net <= 0 ):
+                    fire = 0
+                delta = pArray[2] - fire 
+                for s in range (0,2):
+                    originalArray[s] += delta*alpha*pArray[s]
+                originalArray[2] += delta*alpha
+                TE += abs(delta)
             
-            '''For 1 female       NOTE BELOW THIS'''
-            fire = 0
-            pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
-            pArray2 = []
-            for j in pattern2:
-                pArray2.append(j)
-            net2 = netCalc(originalArray, pArray2)
-            #Activation function
-            if (net2 > 0):
-                fire = 1
-            elif (net2 <= 0 ):
-                fire = 0             
-            delta = pArray2[2] - fire
-            for s in range (0,2):
-                originalArray[s] += delta*alpha*pArray2[s]
-            originalArray[2] += delta*alpha            
-    
+                '''For 1 female       NOTE BELOW THIS'''
+                fire = 0
+                pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
+                pArray2 = []
+                for j in pattern2:
+                    pArray2.append(j)
+                net2 = netCalc(originalArray, pArray2)
+                #Activation function
+                if (net2 > 0):
+                    fire = 1
+                elif (net2 <= 0 ):
+                    fire = 0             
+                delta = pArray2[2] - fire
+                for s in range (0,2):
+                    originalArray[s] += delta*alpha*pArray2[s]
+                originalArray[2] += delta*alpha            
+                TE += abs(delta)
+            print(TE)
+            if(TE<epsilon or bigNum == ni):
+                break
+    return originalArray
+
+'''Can be modified to plot only the non-training data just add a p parameter 1-p'''
+def plotIt(theDF, theEQ):
+    males = theDF[theDF.Gender == 0]
+    females = theDF[theDF.Gender == 1]
     # Plotting the line        
     x = np.linspace(0,1,100)
-    b = -originalArray[2]/originalArray[1]
-    slope = -originalArray[0]/originalArray[1]
+    b = -theEQ[2]/theEQ[1]
+    slope = -theEQ[0]/theEQ[1]
     y = slope*x+b
-    graphIt(femalesA, malesA)
+    graphIt(females, males)
     plt.plot(x,y,'-g')
     plt.show()
     return
-
     
 '''Create Data Objects'''
 dfA = pd.read_csv('groupA.txt', header=None, names = ['Height', 'Weight', 'Gender'])
@@ -165,44 +188,39 @@ dfA = normalizeData(dfA)
 dfB = normalizeData(dfB)
 dfC = normalizeData(dfC)
 
+'''Define Epsilons'''
+epsilonA = 0.00001
+epsilonB = 100
+epsilonC = 1450
 
-'''Splits the original bulk data into two'''
-malesA = dfA[dfA.Gender == 0]
-femalesA = dfA[dfA.Gender == 1]
-malesB = dfB[dfB.Gender == 0]
-femalesB = dfB[dfB.Gender == 1]
-malesC = dfC[dfC.Gender == 0]
-femalesC = dfC[dfC.Gender == 1]
 
 # Alpha should be dynamic
-alpha = 0.8
+alphaAB = 0.8
 ni = 5000 #stopping criteria
 # Gain should be dynamic 
-gain = 10
+gain2 = 50
 ourArray = []
+
 ourArray = initArray(ourArray) #quick method to initialize array with 3 values between (-0.5,0.5)
 
 '''Hard Activation Function'''
-#pass it the array of weights, we pass it the data set row, 
-#Calculate the net, compare it to some value
-
-# New Array = Do alpha times x (which is our array of weights) times (d - o) from activation function
-# weights = old weights plus the newArray
-weightsHAFA = HAF(ourArray, dfA, 0.75)
-weightsHAFA = HAF(ourArray, dfB, 0.75)
-weightsHAFA = HAF(ourArray, dfC, 0.75)
-
+'''
+weightsHAFA = HAF(ourArray, dfA, 0.75, epsilonA, 0.05)
+plotIt(dfA,weightsHAFA)
+weightsHAFB = HAF(ourArray, dfB, 0.75, epsilonB, 0.05)
+plotIt(dfB,weightsHAFB)
+weightsHAFC = HAF(ourArray, dfC, 0.75, epsilonC, 0.001)
+plotIt(dfC,weightsHAFC)
+'''
 #SAF(ourArray,dfA,0.75)
+weightsSAFC = SAF(initArray(ourArray), dfC, 1, 10, epsilonC, 0.05)
+plotIt(dfC, weightsSAFC)
 
+weightsSAFB = SAF(ourArray, dfB, 1, 10, epsilonB, 0.05)
+plotIt(dfB, weightsSAFB)
+weightsSAFA = SAF(ourArray, dfA, 1, 10, epsilonA, 0.05)
+plotIt(dfA, weightsSAFA)
 
-'''
-graphIt(femalesA, malesA)
-plt.show()    
-graphIt(femalesB, malesB)
-plt.show()    
-graphIt(femalesC, malesC)
-plt.show()    
-'''
 
     
 
