f01c28aa7164a0be1f0f48705ef75a1d6bbd7d2d
Peter J George <42278219+pjgeorge09@users.noreply.github.com>
2019-10-02 10:23:43 -0400

    HAF/SAF fully working
    I got the SAF to fully work. 
    Issue solved : Epsilon value being passed as alpha, vice versa. initArray appending to last array instead of new array. New array created before each method.
    
    currently parsed for 75% data and 25% data but NOT finished

--- part2.py
+++ part2.py
@@ -15,8 +15,8 @@ def normalizeData (aPanda):
     maxW = aPanda['Weight'].max()
     minH = aPanda['Height'].min()
     minW = aPanda['Weight'].min()
-    aPanda['Height'] = (aPanda['Height'] - minH) / (maxH - minH)
-    aPanda['Weight'] = (aPanda['Weight'] - minW) / (maxW - minW)
+    aPanda['Height'] = round((aPanda['Height'] - minH) / (maxH - minH),6)
+    aPanda['Weight'] = round((aPanda['Weight'] - minW) / (maxW - minW),6)
     return aPanda;
 
 '''A method to plot the graphs based on inputs'''
@@ -26,7 +26,8 @@ def graphIt (femaleData, maleData):
     '''Find the line here, nested, using Hebbian'''
     return;
 
-def initArray(anArray):
+def initArray():
+    anArray = []
     for i in range(0,3):
         anArray.append(random.uniform(-0.5,0.5))
         anArray[i] = round(anArray[i],6)
@@ -43,29 +44,31 @@ def netCalc(array1, array2):
     return sum
 
 
-def SAF(originalArray, trainingSet, p, epsilon, alpha, gain):
+def SAF(trainingSet, p, epsilon, alpha, gain):
+    originalArray = initArray()
     ni = 5000
     fire = 0 #initialize for reset/global use in function, "did neuron fire"
-    r,c = trainingSet.shape
-    #r = r/2 #Doing one male one female each time in for loop
-    #r = round(r * p) #Make sure it's an int
+    #r,c = trainingSet.shape
+    r=4000
+    r = r/2 #Doing one male one female each time in for loop
+    r = round(r * p) #Make sure it's an int
     u = 2000 # To offset for female sampling
     # It will always do this exactly one time for a male, and one time for a female
     TE = 2000
     while(TE > epsilon):
-        for bigNum in range (0,500):
+        for bigNum in range (0,ni):
             TE = 0
-            print(bigNum)
             #print(str(originalArray[0]) + "X * " + str(originalArray[1]) + "Y + " + str(originalArray[2]) + " > 0")  
-            for x in range(0,1999): #statically trying different values for data update
+            for x in range(0,r): #statically trying different values for data update
                 fire = 0
                 '''For 1 male'''
                 #ex = random.randrange(0,1999)
-                pattern = trainingSet.iloc[x] #should be one row of the passed data set
+                #pattern = trainingSet.iloc[x] #should be one row of the passed data set
+                pattern = trainingSet[x]
                 #this block just makes the row into an array
                 pArray = []
                 for j in pattern:
-                    pArray.append(j)
+                    pArray.append(pattern[j])
                 # net is  the net from slides, where it's >= -1  (midslide pp07)
                 net = netCalc(originalArray,pArray)
                 try:
@@ -81,10 +84,11 @@ def SAF(originalArray, trainingSet, p, epsilon, alpha, gain):
             
                 '''For 1 female       NOTE BELOW THIS'''
                 fire = 0
-                pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
+                #pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
+                pattern2 = trainingSet[x+u]
                 pArray2 = []
                 for j in pattern2:
-                    pArray2.append(j)
+                    pArray2.append(pattern2[j])
                 net2 = netCalc(originalArray,pArray2)
                 try:
                     out = 1/(1+(math.exp(-gain*net2)))
@@ -96,8 +100,7 @@ def SAF(originalArray, trainingSet, p, epsilon, alpha, gain):
                     originalArray[s] += delta*alpha*pArray2[s]
                 originalArray[2] += delta*alpha            
                 TE += delta**2
-            print(TE)
-            print(str(TE) + " < " + str(epsilon))   
+            TE = round(TE,6)
             if(TE<epsilon or bigNum == 99):
                 return originalArray
     return originalArray
@@ -108,12 +111,14 @@ def SAF(originalArray, trainingSet, p, epsilon, alpha, gain):
 ''' @input originalArray = is the starting weight array, initialized elsewhere
     @input trainingSet = is the set of data, dfA,dfB,dfC for us in our work
     @input p = is the percent of the data you want to use as training data'''
-def HAF(originalArray, trainingSet, p, epsilon, alpha):
+def HAF(trainingSet, p, epsilon, alpha):
+    originalArray = initArray()
     ni = 5000
     fire = 0 #initialize for reset/global use in function, "did neuron fire"
-    r,c = trainingSet.shape
-    #r = r/2 #Doing one male one female each time in for loop
-    #r = round(r * p) #Make sure it's an int
+    r = 4000
+    #r,c = trainingSet.shape
+    r = r/2 #Doing one male one female each time in for loop
+    r = round(r * p) #Make sure it's an int
     u = 2000 # To offset for female sampling
     # It will always do this exactly one time for a male, and one time for a female
     TE = 2000
@@ -121,17 +126,17 @@ def HAF(originalArray, trainingSet, p, epsilon, alpha):
         TE = 0
         for bigNum in range (0,ni):
             TE = 0
-            print(bigNum)
             #print(str(originalArray[0]) + "X * " + str(originalArray[1]) + "Y + " + str(originalArray[2]) + " > 0")  
-            for x in range(0,1999): #statically trying different values for data update
+            for x in range(0,r): #statically trying different values for data update
                 fire = 0
                 '''For 1 male'''
                 #ex = random.randrange(0,1999)
-                pattern = trainingSet.iloc[x] #should be one row of the passed data set
+                #pattern = trainingSet.iloc[x] #should be one row of the passed data set
+                pattern = trainingSet[x]
                 #this block just makes the row into an array
                 pArray = []
                 for j in pattern:
-                    pArray.append(j)
+                    pArray.append(pattern[j])
                 # net is  the net from slides, where it's >= -1  (midslide pp07)
                 net = netCalc(originalArray, pArray) #USE PP4 SLIDE 3,
                 #Activation function
@@ -147,10 +152,11 @@ def HAF(originalArray, trainingSet, p, epsilon, alpha):
             
                 '''For 1 female       NOTE BELOW THIS'''
                 fire = 0
-                pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
+                #pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
+                pattern2 = trainingSet[x+u]
                 pArray2 = []
                 for j in pattern2:
-                    pArray2.append(j)
+                    pArray2.append(pattern2[j])
                 net2 = netCalc(originalArray, pArray2)
                 #Activation function
                 if (net2 > 0):
@@ -162,15 +168,14 @@ def HAF(originalArray, trainingSet, p, epsilon, alpha):
                     originalArray[s] += delta*alpha*pArray2[s]
                 originalArray[2] += delta*alpha            
                 TE += abs(delta)
-            print(TE)
             if(TE<epsilon or bigNum == ni):
                    return originalArray
     return originalArray
 
 '''Can be modified to plot only the non-training data just add a p parameter 1-p'''
-def plotIt(theDF, theEQ):
-    males = theDF[theDF.Gender == 0]
-    females = theDF[theDF.Gender == 1]
+def plotIt(theDF, theDFo, theEQ):
+    males = theDFo[theDFo.Gender == 0]
+    females = theDFo[theDFo.Gender == 1]
     # Plotting the line        
     x = np.linspace(0,1,100)
     b = -theEQ[2]/theEQ[1]
@@ -182,48 +187,59 @@ def plotIt(theDF, theEQ):
     return
     
 '''Create Data Objects'''
-dfA = pd.read_csv('groupA.txt', header=None, names = ['Height', 'Weight', 'Gender'])
-dfB = pd.read_csv('groupB.txt', header=None, names = ['Height', 'Weight', 'Gender'])
-dfC = pd.read_csv('groupC.txt', header=None, names = ['Height', 'Weight', 'Gender'])
-
+dfAo = pd.read_csv('groupA.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfBo = pd.read_csv('groupB.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfCo = pd.read_csv('groupC.txt', header=None, names = ['Height', 'Weight', 'Gender'])
 '''Normalize Data'''
-dfA = normalizeData(dfA)
-dfB = normalizeData(dfB)
-dfC = normalizeData(dfC)
+dfAo = normalizeData(dfAo)
+dfBo = normalizeData(dfBo)
+dfCo = normalizeData(dfCo)
+dfA = dfAo.to_dict('index')
+dfB = dfBo.to_dict('index')
+dfC = dfCo.to_dict('index')
 
+print(dfA[0])
+thing = dfA[0]
+for _ in thing:
+    print(thing[_])
 '''Define Epsilons'''
 epsilonA = 0.00001
 epsilonB = 100
 epsilonC = 1450
 
+'''Hard Activation Function at 75% training'''
+ht75 = 0.75
+weightsHAFA = HAF(dfA, ht75, epsilonA, 0.5)
+plotIt(dfA, dfAo, weightsHAFA)
+weightsHAFB = HAF(dfB, ht75, epsilonB, 0.5)
+plotIt(dfB, dfBo, weightsHAFB)
+weightsHAFC = HAF(dfC, ht75, epsilonC, 0.01)
+plotIt(dfC, dfCo, weightsHAFC)
 
-# Alpha should be dynamic
-alphaAB = 0.8
-ni = 5000 #stopping criteria
-# Gain should be dynamic 
-gain2 = 50
-ourArray = []
+'''Soft Activation Function at 75% training'''
+st75 = 0.75
+weightsSAFA = SAF(dfA, st75, epsilonA, 0.5, 5)
+plotIt(dfA, dfAo, weightsSAFA)
+weightsSAFB = SAF(dfB, st75, epsilonB, 0.5, 5)
+plotIt(dfB, dfBo, weightsSAFB)
+weightsSAFC = SAF(dfC, st75, epsilonC, 0.5, 5)
+plotIt(dfC, dfCo, weightsSAFC)
 
-ourArray = initArray(ourArray) #quick method to initialize array with 3 values between (-0.5,0.5)
+'''Hard Activation Function at 25% training'''
+ht25 = 0.25
+weightsHAFA = HAF(dfA, ht25, epsilonA, 0.5)
+plotIt(dfA, dfAo, weightsHAFA)
+weightsHAFB = HAF(dfB, ht25, epsilonB, 0.5)
+plotIt(dfB, dfBo, weightsHAFB)
+weightsHAFC = HAF(dfC, ht25, epsilonC, 0.5)
+plotIt(dfC, dfCo, weightsHAFC)
 
-'''Hard Activation Function'''
-
-'''weightsHAFA = HAF(ourArray, dfA, 1, epsilonA, 0.5)
-plotIt(dfA,weightsHAFA)
-weightsHAFB = HAF(ourArray, dfB, 1, epsilonB, 0.5)
-plotIt(dfB,weightsHAFB)
-weightsHAFC = HAF(ourArray, dfC, 1, epsilonC, 0.0001)
-plotIt(dfC,weightsHAFC)'''
-
-#SAF(ourArray,dfA,0.75)
-weightsSAFC = SAF(initArray(ourArray), dfC, 1, 0.5, epsilonC, 0.8)
-plotIt(dfC, weightsSAFC)
-
-weightsSAFB = SAF(ourArray, dfB, 1, 1, epsilonB, 0.8)
-plotIt(dfB, weightsSAFB)
-weightsSAFA = SAF(ourArray, dfA, 1, 1, epsilonA, 0.8)
-plotIt(dfA, weightsSAFA)
-
-
-    
+'''Soft Activation Function at 25% training'''
+st25 = 0.25
+weightsSAFA = SAF(dfA, st25, epsilonA, 0.5, 5)
+plotIt(dfA, dfAo, weightsSAFA)
+weightsSAFB = SAF(dfB, st25, epsilonB, 0.5, 5)
+plotIt(dfB, dfBo, weightsSAFB)
+weightsSAFC = SAF(dfC, st25, epsilonC, 0.5, 5)
+plotIt(dfC, dfCo, weightsSAFC)
 
