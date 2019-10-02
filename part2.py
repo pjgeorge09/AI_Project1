385cb3f57440592dbc51699693e316491ff625fb
Peter J George <42278219+pjgeorge09@users.noreply.github.com>
2019-10-02 11:22:06 -0400

    update
    Moved graphIt method next to plotIt method, added axis and labels. Might need to un-normalize data?
    tidied up a bit.

--- part2.py
+++ part2.py
@@ -26,6 +26,23 @@ def graphIt (femaleData, maleData):
     '''Find the line here, nested, using Hebbian'''
     return;
 
+'''Can be modified to plot only the non-training data just add a p parameter 1-p'''
+def plotIt(theDF, theDFo, theEQ):
+    males = theDFo[theDFo.Gender == 0]
+    females = theDFo[theDFo.Gender == 1]
+    # Plotting the line        
+    x = np.linspace(0,1,100)
+    b = -theEQ[2]/theEQ[1]
+    slope = -theEQ[0]/theEQ[1]
+    y = slope*x+b
+    graphIt(females, males)
+    plt.plot(x,y,'-g')
+    plt.axis([0,1.1,0,1.1])
+    plt.xlabel('Heights, normalized')
+    plt.ylabel('Weights, normalized')
+    plt.show()
+    return
+
 def initArray():
     anArray = []
     for i in range(0,3):
@@ -46,24 +63,17 @@ def netCalc(array1, array2):
 
 def SAF(trainingSet, p, epsilon, alpha, gain):
     originalArray = initArray()
-    ni = 5000
-    fire = 0 #initialize for reset/global use in function, "did neuron fire"
-    #r,c = trainingSet.shape
-    r=4000
+    r = len(trainingSet)
     r = r/2 #Doing one male one female each time in for loop
     r = round(r * p) #Make sure it's an int
-    u = 2000 # To offset for female sampling
+    offset = 2000 # To offset for female sampling
     # It will always do this exactly one time for a male, and one time for a female
     TE = 2000
     while(TE > epsilon):
         for bigNum in range (0,ni):
             TE = 0
-            #print(str(originalArray[0]) + "X * " + str(originalArray[1]) + "Y + " + str(originalArray[2]) + " > 0")  
             for x in range(0,r): #statically trying different values for data update
-                fire = 0
                 '''For 1 male'''
-                #ex = random.randrange(0,1999)
-                #pattern = trainingSet.iloc[x] #should be one row of the passed data set
                 pattern = trainingSet[x]
                 #this block just makes the row into an array
                 pArray = []
@@ -71,31 +81,22 @@ def SAF(trainingSet, p, epsilon, alpha, gain):
                     pArray.append(pattern[j])
                 # net is  the net from slides, where it's >= -1  (midslide pp07)
                 net = netCalc(originalArray,pArray)
-                try:
-                    out = 1/(1+(math.exp(-1*gain*net)))
-                except OverflowError:
-                    out = pArray[2]
-                fire = out
-                delta = pArray[2] - fire 
+                out = 1/(1+(math.exp(-1*gain*net)))
+                delta = pArray[2] - out 
                 for s in range (0,2):
                     originalArray[s] += delta*alpha*pArray[s]
                 originalArray[2] += delta*alpha
                 TE += delta**2
             
                 '''For 1 female       NOTE BELOW THIS'''
-                fire = 0
                 #pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
-                pattern2 = trainingSet[x+u]
+                pattern2 = trainingSet[x+offset]
                 pArray2 = []
                 for j in pattern2:
                     pArray2.append(pattern2[j])
                 net2 = netCalc(originalArray,pArray2)
-                try:
-                    out = 1/(1+(math.exp(-gain*net2)))
-                except OverflowError:
-                    out = pArray2[2]
-                fire = out
-                delta = pArray2[2] - fire
+                out = 1/(1+(math.exp(-gain*net2)))
+                delta = pArray2[2] - out
                 for s in range (0,2):
                     originalArray[s] += delta*alpha*pArray2[s]
                 originalArray[2] += delta*alpha            
@@ -112,26 +113,21 @@ def SAF(trainingSet, p, epsilon, alpha, gain):
     @input trainingSet = is the set of data, dfA,dfB,dfC for us in our work
     @input p = is the percent of the data you want to use as training data'''
 def HAF(trainingSet, p, epsilon, alpha):
-    originalArray = initArray()
-    ni = 5000
-    fire = 0 #initialize for reset/global use in function, "did neuron fire"
-    r = 4000
-    #r,c = trainingSet.shape
-    r = r/2 #Doing one male one female each time in for loop
-    r = round(r * p) #Make sure it's an int
-    u = 2000 # To offset for female sampling
-    # It will always do this exactly one time for a male, and one time for a female
-    TE = 2000
+    originalArray = initArray()                                                 #random array len(3) range(-0.5,0.5)
+    fire = 0                                                                    #initialize for reset/global use in function, "did neuron fire"
+    r = len(trainingSet)                                                        #num of rows
+    r = r/2                                                                     #Doing one male one female each time in for loop
+    r = round(r * p)                                                            #Make sure it's an int
+    offset = 2000                                                                    #To offset for female sampling
+    TE = 2000                                                                   #init Total Error above threshold to enter while loop
     while(TE > epsilon):
         TE = 0
         for bigNum in range (0,ni):
             TE = 0
-            #print(str(originalArray[0]) + "X * " + str(originalArray[1]) + "Y + " + str(originalArray[2]) + " > 0")  
-            for x in range(0,r): #statically trying different values for data update
+            '''For each x value, it will run data on one male and one female'''
+            for x in range(0,r): 
                 fire = 0
                 '''For 1 male'''
-                #ex = random.randrange(0,1999)
-                #pattern = trainingSet.iloc[x] #should be one row of the passed data set
                 pattern = trainingSet[x]
                 #this block just makes the row into an array
                 pArray = []
@@ -152,8 +148,7 @@ def HAF(trainingSet, p, epsilon, alpha):
             
                 '''For 1 female       NOTE BELOW THIS'''
                 fire = 0
-                #pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
-                pattern2 = trainingSet[x+u]
+                pattern2 = trainingSet[x+offset]
                 pArray2 = []
                 for j in pattern2:
                     pArray2.append(pattern2[j])
@@ -172,74 +167,59 @@ def HAF(trainingSet, p, epsilon, alpha):
                    return originalArray
     return originalArray
 
-'''Can be modified to plot only the non-training data just add a p parameter 1-p'''
-def plotIt(theDF, theDFo, theEQ):
-    males = theDFo[theDFo.Gender == 0]
-    females = theDFo[theDFo.Gender == 1]
-    # Plotting the line        
-    x = np.linspace(0,1,100)
-    b = -theEQ[2]/theEQ[1]
-    slope = -theEQ[0]/theEQ[1]
-    y = slope*x+b
-    graphIt(females, males)
-    plt.plot(x,y,'-g')
-    plt.show()
-    return
     
 '''Create Data Objects'''
-dfAo = pd.read_csv('groupA.txt', header=None, names = ['Height', 'Weight', 'Gender'])
-dfBo = pd.read_csv('groupB.txt', header=None, names = ['Height', 'Weight', 'Gender'])
-dfCo = pd.read_csv('groupC.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfAx = pd.read_csv('groupA.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfBx = pd.read_csv('groupB.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfCx = pd.read_csv('groupC.txt', header=None, names = ['Height', 'Weight', 'Gender'])
 '''Normalize Data'''
-dfAo = normalizeData(dfAo)
-dfBo = normalizeData(dfBo)
-dfCo = normalizeData(dfCo)
+dfAo = normalizeData(dfAx)
+dfBo = normalizeData(dfBx)
+dfCo = normalizeData(dfCx)
+'''Turn DF objects to lists for speed'''
 dfA = dfAo.to_dict('index')
 dfB = dfBo.to_dict('index')
 dfC = dfCo.to_dict('index')
 
-print(dfA[0])
-thing = dfA[0]
-for _ in thing:
-    print(thing[_])
-'''Define Epsilons'''
+'''Define Variables'''
 epsilonA = 0.00001
 epsilonB = 100
 epsilonC = 1450
+ni = 5000   #Stopping criteria
+ht75 = 0.75 #Each of these 4 : h = hard, t = training , number(75/25) = percent training 
+st75 = 0.75
+ht25 = 0.25
+st25 = 0.25
 
 '''Hard Activation Function at 75% training'''
-ht75 = 0.75
 weightsHAFA = HAF(dfA, ht75, epsilonA, 0.5)
 plotIt(dfA, dfAo, weightsHAFA)
 weightsHAFB = HAF(dfB, ht75, epsilonB, 0.5)
 plotIt(dfB, dfBo, weightsHAFB)
-weightsHAFC = HAF(dfC, ht75, epsilonC, 0.01)
+weightsHAFC = HAF(dfC, ht75, epsilonC, 0.25)
 plotIt(dfC, dfCo, weightsHAFC)
 
 '''Soft Activation Function at 75% training'''
-st75 = 0.75
 weightsSAFA = SAF(dfA, st75, epsilonA, 0.5, 5)
 plotIt(dfA, dfAo, weightsSAFA)
 weightsSAFB = SAF(dfB, st75, epsilonB, 0.5, 5)
 plotIt(dfB, dfBo, weightsSAFB)
-weightsSAFC = SAF(dfC, st75, epsilonC, 0.5, 5)
+weightsSAFC = SAF(dfC, st75, epsilonC, 0.1, 5)
 plotIt(dfC, dfCo, weightsSAFC)
 
 '''Hard Activation Function at 25% training'''
-ht25 = 0.25
 weightsHAFA = HAF(dfA, ht25, epsilonA, 0.5)
 plotIt(dfA, dfAo, weightsHAFA)
 weightsHAFB = HAF(dfB, ht25, epsilonB, 0.5)
 plotIt(dfB, dfBo, weightsHAFB)
-weightsHAFC = HAF(dfC, ht25, epsilonC, 0.5)
+weightsHAFC = HAF(dfC, ht25, epsilonC, 0.25)
 plotIt(dfC, dfCo, weightsHAFC)
 
 '''Soft Activation Function at 25% training'''
-st25 = 0.25
 weightsSAFA = SAF(dfA, st25, epsilonA, 0.5, 5)
 plotIt(dfA, dfAo, weightsSAFA)
 weightsSAFB = SAF(dfB, st25, epsilonB, 0.5, 5)
 plotIt(dfB, dfBo, weightsSAFB)
-weightsSAFC = SAF(dfC, st25, epsilonC, 0.5, 5)
+weightsSAFC = SAF(dfC, st25, epsilonC, 0.1, 5)
 plotIt(dfC, dfCo, weightsSAFC)
 
