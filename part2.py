3c60c3da372a58e3c2c212bf45c14c1ef8b2ecab
Peter J George <42278219+pjgeorge09@users.noreply.github.com>
2019-10-02 21:27:04 -0400

    .
    Merged Daniel's methods for testing, fixed backwards numbers, eliminated unnecessary code, beautified graphs, some comments, some reorganization

--- part2.py
+++ part2.py
@@ -21,13 +21,14 @@ def normalizeData (aPanda):
 
 '''A method to plot the graphs based on inputs'''
 def graphIt (femaleData, maleData):
-    plt.scatter(femaleData['Weight'], femaleData['Height'], color='r', marker='.')
-    plt.scatter(maleData['Weight'], maleData['Height'], color='b', marker='.')
+    plt.scatter(femaleData['Weight'], femaleData['Height'], color='r', marker='v', s=7, label="Female", alpha=0.5)
+    plt.scatter(maleData['Weight'], maleData['Height'], color='b', marker='.', s=7, label="Male", alpha=0.5)
+    plt.legend()
     '''Find the line here, nested, using Hebbian'''
     return;
 
 '''Can be modified to plot only the non-training data just add a p parameter 1-p'''
-def plotIt(theDF, theDFo, theEQ):
+def plotIt(theDF, theDFo, theEQ, graphTitle):
     males = theDFo[theDFo.Gender == 0]
     females = theDFo[theDFo.Gender == 1]
     # Plotting the line        
@@ -40,24 +41,24 @@ def plotIt(theDF, theDFo, theEQ):
     plt.axis([0,1.1,0,1.1])
     plt.xlabel('Heights, normalized')
     plt.ylabel('Weights, normalized')
+    plt.title(graphTitle, loc='center')
     plt.show()
     return
 
+'''A method to initialize arrays with 3 values within the range below, returns that random array'''
 def initArray():
     anArray = []
     for i in range(0,3):
         anArray.append(random.uniform(-0.5,0.5))
-        anArray[i] = round(anArray[i],6)
     return anArray
 
-# Section 7, slide 7 Hard Activation Net Value
+'''A method that calculates the net where net is (Σ(W_i)(X_i)) + bias'''
 def netCalc(array1, array2):
     sum = 0
     for i in range (0,2):                           #Dot Product & round
         sum = sum + array1[i]*array2[i]
     sum += array1[2]                         #Adding in the bias
-    # If this value is greater than zero , fire neuron otherwise don't
-    
+    # If this value is greater than zero , fire neuron otherwise don't (for HAF)
     return sum
 
 
@@ -89,20 +90,20 @@ def SAF(trainingSet, p, epsilon, alpha, gain):
                 TE += delta**2
             
                 '''For 1 female       NOTE BELOW THIS'''
-                #pattern2 = trainingSet.iloc[x+u] #should be one row of the passed data set on FEMALE side
                 pattern2 = trainingSet[x+offset]
                 pArray2 = []
                 for j in pattern2:
                     pArray2.append(pattern2[j])
+                    
                 net2 = netCalc(originalArray,pArray2)
                 out = 1/(1+(math.exp(-gain*net2)))
+                
                 delta = pArray2[2] - out
                 for s in range (0,2):
                     originalArray[s] += delta*alpha*pArray2[s]
                 originalArray[2] += delta*alpha            
                 TE += delta**2
-            TE = round(TE,6)
-            if(TE<epsilon or bigNum == 99):
+            if(TE<epsilon):
                 return originalArray
     return originalArray
 
@@ -113,19 +114,19 @@ def SAF(trainingSet, p, epsilon, alpha, gain):
     @input trainingSet = is the set of data, dfA,dfB,dfC for us in our work
     @input p = is the percent of the data you want to use as training data'''
 def HAF(trainingSet, p, epsilon, alpha):
-    originalArray = initArray()                                                 #random array len(3) range(-0.5,0.5)
-    fire = 0                                                                    #initialize for reset/global use in function, "did neuron fire"
-    r = len(trainingSet)                                                        #num of rows
-    r = r/2                                                                     #Doing one male one female each time in for loop
-    r = round(r * p)                                                            #Make sure it's an int
-    offset = 2000                                                                    #To offset for female sampling
-    TE = 2000                                                                   #init Total Error above threshold to enter while loop
+    originalArray = initArray()
+    fire = 0 #initialize for reset/global use in function, "did neuron fire"
+    r = len(trainingSet)
+    r = r/2 #Doing one male one female each time in for loop
+    r = round(r * p) #Make sure it's an int
+    offset = 2000 # To offset for female sampling
+    # It will always do this exactly one time for a male, and one time for a female
+    TE = 2000
     while(TE > epsilon):
         TE = 0
         for bigNum in range (0,ni):
             TE = 0
-            '''For each x value, it will run data on one male and one female'''
-            for x in range(0,r): 
+            for x in range(0,r): #statically trying different values for data update
                 fire = 0
                 '''For 1 male'''
                 pattern = trainingSet[x]
@@ -133,7 +134,6 @@ def HAF(trainingSet, p, epsilon, alpha):
                 pArray = []
                 for j in pattern:
                     pArray.append(pattern[j])
-                # net is  the net from slides, where it's >= -1  (midslide pp07)
                 net = netCalc(originalArray, pArray) #USE PP4 SLIDE 3,
                 #Activation function
                 if (net > 0):
@@ -163,63 +163,142 @@ def HAF(trainingSet, p, epsilon, alpha):
                     originalArray[s] += delta*alpha*pArray2[s]
                 originalArray[2] += delta*alpha            
                 TE += abs(delta)
-            if(TE<epsilon or bigNum == ni):
+            if(TE<epsilon):
                    return originalArray
     return originalArray
 
     
 '''Create Data Objects'''
-dfAx = pd.read_csv('groupA.txt', header=None, names = ['Height', 'Weight', 'Gender'])
-dfBx = pd.read_csv('groupB.txt', header=None, names = ['Height', 'Weight', 'Gender'])
-dfCx = pd.read_csv('groupC.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfAn = pd.read_csv('groupA.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfBn = pd.read_csv('groupB.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+dfCn = pd.read_csv('groupC.txt', header=None, names = ['Height', 'Weight', 'Gender'])
+
 '''Normalize Data'''
-dfAo = normalizeData(dfAx)
-dfBo = normalizeData(dfBx)
-dfCo = normalizeData(dfCx)
+dfAo = normalizeData(dfAn)
+dfBo = normalizeData(dfBn)
+dfCo = normalizeData(dfCn)
+
 '''Turn DF objects to lists for speed'''
+dfAn = dfAn.to_dict('index')
+dfBn = dfBn.to_dict('index')
+dfCn = dfCn.to_dict('index')
+
 dfA = dfAo.to_dict('index')
 dfB = dfBo.to_dict('index')
 dfC = dfCo.to_dict('index')
+
 
 '''Define Variables'''
 epsilonA = 0.00001
 epsilonB = 100
 epsilonC = 1450
-ni = 5000   #Stopping criteria
-ht75 = 0.75 #Each of these 4 : h = hard, t = training , number(75/25) = percent training 
-st75 = 0.75
-ht25 = 0.25
-st25 = 0.25
+ni = 5000
 
 '''Hard Activation Function at 75% training'''
+ht75 = 0.75
 weightsHAFA = HAF(dfA, ht75, epsilonA, 0.5)
-plotIt(dfA, dfAo, weightsHAFA)
+plotIt(dfA, dfAo, weightsHAFA, "HAF Train on 75% of A")
 weightsHAFB = HAF(dfB, ht75, epsilonB, 0.5)
-plotIt(dfB, dfBo, weightsHAFB)
-weightsHAFC = HAF(dfC, ht75, epsilonC, 0.25)
-plotIt(dfC, dfCo, weightsHAFC)
+plotIt(dfB, dfBo, weightsHAFB, "HAF Train on 75% of B")
+weightsHAFC = HAF(dfC, ht75, epsilonC, 0.01)
+plotIt(dfC, dfCo, weightsHAFC, "HAF Train on 75% of C")
 
 '''Soft Activation Function at 75% training'''
+st75 = 0.75
 weightsSAFA = SAF(dfA, st75, epsilonA, 0.5, 5)
-plotIt(dfA, dfAo, weightsSAFA)
+plotIt(dfA, dfAo, weightsSAFA, "SAF Train on 75% of A")
 weightsSAFB = SAF(dfB, st75, epsilonB, 0.5, 5)
-plotIt(dfB, dfBo, weightsSAFB)
-weightsSAFC = SAF(dfC, st75, epsilonC, 0.1, 5)
-plotIt(dfC, dfCo, weightsSAFC)
+plotIt(dfB, dfBo, weightsSAFB, "SAF Train on 75% of B")
+weightsSAFC = SAF(dfC, st75, epsilonC, 0.5, 5)
+plotIt(dfC, dfCo, weightsSAFC, "SAF Train on 75% of C")
 
 '''Hard Activation Function at 25% training'''
+ht25 = 0.25
 weightsHAFA = HAF(dfA, ht25, epsilonA, 0.5)
-plotIt(dfA, dfAo, weightsHAFA)
+plotIt(dfA, dfAo, weightsHAFA, "HAF Train on 25% of A")
 weightsHAFB = HAF(dfB, ht25, epsilonB, 0.5)
-plotIt(dfB, dfBo, weightsHAFB)
-weightsHAFC = HAF(dfC, ht25, epsilonC, 0.25)
-plotIt(dfC, dfCo, weightsHAFC)
+plotIt(dfB, dfBo, weightsHAFB, "HAF Train on 25% of B")
+weightsHAFC = HAF(dfC, ht25, epsilonC, 0.5)
+plotIt(dfC, dfCo, weightsHAFC, "HAF Train on 25% of C")
 
 '''Soft Activation Function at 25% training'''
+st25 = 0.25
 weightsSAFA = SAF(dfA, st25, epsilonA, 0.5, 5)
-plotIt(dfA, dfAo, weightsSAFA)
+plotIt(dfA, dfAo, weightsSAFA, "SAF Train on 25% of A")
 weightsSAFB = SAF(dfB, st25, epsilonB, 0.5, 5)
-plotIt(dfB, dfBo, weightsSAFB)
-weightsSAFC = SAF(dfC, st25, epsilonC, 0.1, 5)
-plotIt(dfC, dfCo, weightsSAFC)
+plotIt(dfB, dfBo, weightsSAFB, "SAF Train on 25% of B")
+weightsSAFC = SAF(dfC, st25, epsilonC, 0.5, 5)
+plotIt(dfC, dfCo, weightsSAFC, "SAF Train on 25% of C")
 
+def calculate_inequal(num,ww):
+   if (num['Height']*ww[0]+ww[1]*num['Weight']+ww[2])>0:
+       return 1
+   else:
+       return 0
+'''Testing Function
+    @input:dataframe,weights from weightsSAF or HAF
+    @output:error for testing with actual and plots for testing'''
+
+def testingFunction(df_train,weights,percnt, stringTitle):
+    r=4000
+    r=r/2
+    r=round(r*percnt)
+    nw=round(len(df_train)/2)
+    rlist=[]
+    for i in range(r,nw):
+        rlist.append(df_train[i])
+    
+    for l in range(nw+r,len(df_train)):
+        rlist.append(df_train[l])
+    
+    
+    df_test=pd.DataFrame(rlist)
+#    print(df_test)
+    df_testPerceptrn=df_test.apply(calculate_inequal,axis=1,ww=(weights))
+#    print(df_testPerceptrn)
+    actual_matrix=df_test['Gender']
+    
+    predicted_matrix=df_testPerceptrn
+    
+    confusion_matrix=pd.crosstab(actual_matrix,predicted_matrix)
+    
+    df = normalizeData(df_test)
+    
+    plotIt(df_test,df,weights,stringTitle)
+    
+    print(confusion_matrix)
+
+    return
+
+#print(males)
+#print(females)
+#testingFunction(dfA,weightsSAFA,0.75)
+
+
+'''Hard Activation Function at 25% testing'''
+print("\nHard Activation Function at 25% testing:\n")
+htst75=0.75
+testingFunction(dfA,weightsHAFA,htst75, "HAF Test on 25% of A")
+testingFunction(dfB,weightsHAFB,htst75, "HAF Test on 25% of B")
+testingFunction(dfC,weightsHAFC,htst75, "HAF Test on 25% of C")
+        
+'''Soft Activation Function at 25% testing'''
+print("\nSoft Activation Function at 25% testing:\n")
+stst75=0.75
+testingFunction(dfA,weightsSAFA,stst75, "SAF Test on 25% of A")
+testingFunction(dfB,weightsSAFB,stst75, "SAF Test on 25% of B")
+testingFunction(dfC,weightsSAFC,stst75, "SAF Test on 25% of C")
+
+'''Hard Activation Function at 75% testing'''
+print("\nHard Activation Function at 75% testing:\n")
+htst25=0.25
+testingFunction(dfA,weightsHAFA,htst25, "HAF Test on 75% of A")
+testingFunction(dfB,weightsHAFB,htst25, "HAF Test on 75% of B")
+testingFunction(dfC,weightsHAFC,htst25, "HAF Test on 75% of C")
+
+'''Soft Activation Function at 75% testing'''
+print("\n\nSoft Activation Function at 75% testing:\n")
+stst25=0.25
+testingFunction(dfA,weightsSAFA,stst25, "SAF Test on 75% of A")
+testingFunction(dfB,weightsSAFB,stst25, "SAF Test on 75% of B")
+testingFunction(dfC,weightsSAFC,stst25, "SAF Test on 75% of C")
